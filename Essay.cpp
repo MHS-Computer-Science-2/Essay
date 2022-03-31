58dd1e1fff0d81667bedf528155402a6e55b1fc5
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2022-03-31 13:19:48 -0400

    Add files via upload

---
+++ Essay.cpp
@@ -0,0 +1,26 @@
+// Essay
+
+#include <iostream>
+using namespace std;
+
+// Create your Essay struct here
+
+
+int main()
+{
+    // Have the user enter values
+
+    
+    // Create an Essay object and set the attributes
+
+
+    //Calculate the number of days needed to complete the essay
+
+
+    // Write if statements to determine if the essay will be completed 
+    // EARLY, ON TIME, or LATE
+
+
+    system("pause");
+}
+
